517ce195211b0ccf98780df3d79a164493407976
anhelov-y <yuriy.anhelov@gmail.com>
2024-10-23 19:24:52 +0300

    Completed_SECOND_task

---
+++ second_task.py
@@ -0,0 +1,23 @@
+import random
+
+#Введення чисел (умови) для вибірки
+min = int(input("Enter MIN number: "))
+max = int(input("Enter MAX number: "))
+quantity = int(input("Enter Quantity of numbers: "))
+
+#Функція з перевіркою чисел та розрахунком
+def get_numbers_ticket(min, max, quantity):
+    # Перевірка заданих чисел min, max, quantity
+    if min < 1:
+        return ("[       ]")
+    if max > 1000:
+        return ("[       ]")
+    if max-min < quantity: 
+        return ("[       ]")
+    # Обчислення і сортування
+    population = range(min,max)
+    lottery_numbers=random.sample(population, quantity)
+    lottery_numbers.sort()
+    return lottery_numbers
+
+print(get_numbers_ticket(min,max,quantity))
